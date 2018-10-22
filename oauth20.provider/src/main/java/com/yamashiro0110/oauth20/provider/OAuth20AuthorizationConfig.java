60d94b845944e62f0fd9682e80d2eb269aed57b2
r_yamashiro <r_yamashiro@fancs.com>
2018-10-22 18:20:21 +0900

    add OAuth20AuthorizationConfig

---
+++ oauth20.provider/src/main/java/com/yamashiro0110/oauth20/provider/OAuth20AuthorizationConfig.java
@@ -0,0 +1,28 @@
+package com.yamashiro0110.oauth20.provider;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth20AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.setClientDetailsService(this.clientDetailsService);
+    }
+}
