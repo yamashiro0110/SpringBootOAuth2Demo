89e8535382ab5da8875c0b2d5664f45f543cda33
r_yamashiro <r_yamashiro@fancs.com>
2018-10-23 14:48:06 +0900

    refactor provider config

--- oauth20.provider/src/main/java/com/yamashiro0110/oauth20/provider/AuthorizationConfig.java
+++ oauth20.provider/src/main/java/com/yamashiro0110/oauth20/provider/AuthorizationConfig.java
@@ -1,28 +1,40 @@
 package com.yamashiro0110.oauth20.provider;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
+import javax.annotation.Resource;
+
 @Configuration
 @EnableAuthorizationServer
-public class OAuth20AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
-    @Autowired
+public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+    @Resource(name = "customClientDetailsService")
     private ClientDetailsService clientDetailsService;
 
     @Bean
-    public TokenStore tokenStore() {
+    TokenStore tokenStore() {
         return new InMemoryTokenStore();
     }
 
     @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.setClientDetailsService(this.clientDetailsService);
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.tokenStore(this.tokenStore());
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.withClientDetails(this.clientDetailsService);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) {
     }
 }
--- oauth20.provider/src/main/java/com/yamashiro0110/oauth20/provider/CustomClientDetailsService.java
+++ oauth20.provider/src/main/java/com/yamashiro0110/oauth20/provider/CustomClientDetailsService.java
@@ -8,29 +8,41 @@ import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
-@Service
+@Service("customClientDetailsService")
 public class CustomClientDetailsService implements ClientDetailsService {
     @Override
     public ClientDetails loadClientByClientId(String clientId) throws ClientRegistrationException {
         BaseClientDetails clientDetails = new BaseClientDetails();
         clientDetails.setClientId(clientId);
         clientDetails.setClientSecret("sample-client-secret");
-        clientDetails.setScope(Arrays.asList("read", "write"));
-        clientDetails.setResourceIds(Arrays.asList("resource_1", "resource_2"));
-        clientDetails.setAuthorizedGrantTypes(Arrays.asList("authorization_code"));
+        clientDetails.setScope(this.scopes());
+        clientDetails.setResourceIds(this.resourceIds());
+        clientDetails.setAuthorizedGrantTypes(this.authorizedGrantTypes());
         clientDetails.setRegisteredRedirectUri(this.redirectUrls());
-        clientDetails.setAuthorizedGrantTypes(Arrays.asList("user"));
         clientDetails.setAccessTokenValiditySeconds(3600);
         clientDetails.setRefreshTokenValiditySeconds(3600 * 24 * 90);
         return clientDetails;
+    }
+
+    private List<String> resourceIds() {
+        return Arrays.asList("resource_1", "resource_2");
     }
 
     private Set<String> redirectUrls() {
         Set<String> redirectUrl = new LinkedHashSet<>();
         redirectUrl.add("http://localhost:8100");
         return redirectUrl;
+    }
+
+    private List<String> authorizedGrantTypes() {
+        return Arrays.asList("authorization_code");
+    }
+
+    private List<String> scopes() {
+        return Arrays.asList("read", "write");
     }
 
 }
