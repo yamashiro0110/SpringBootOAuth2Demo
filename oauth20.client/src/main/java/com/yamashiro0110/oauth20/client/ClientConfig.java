2d259fb2cc28d73a9c960da692e36a93581053e8
yamashiro.ryota <yamashiro0110@gmail.com>
2018-11-06 00:49:07 +0900

    update oauth client config

--- oauth20.client/src/main/java/com/yamashiro0110/oauth20/client/ClientConfig.java
+++ oauth20.client/src/main/java/com/yamashiro0110/oauth20/client/ClientConfig.java
@@ -1,5 +1,6 @@
 package com.yamashiro0110.oauth20.client;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,30 +9,55 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableOAuth2Client
+@Data
 public class ClientConfig extends WebSecurityConfigurerAdapter {
     @Value("${oauth2.demo.authorizationServerEndpoint:http://localhost:8100/provider}")
     private String authorizationServerEndpoint;
+    @Value("${oauth2.client.demo.resourceId:resource_1}")
+    private String resourceId;
+    @Value("${oauth2.client.demo.clientId:client_id}")
+    private String clientId;
+    @Value("${oauth2.client.demo.clientSecret:client_secret}")
+    private String clientSecret;
+    @Value("${oauth2.client.demo.scope:read,write}")
+    private String[] scope;
+
+    private List<String> scopes() {
+        return Arrays.asList(this.scope);
+    }
 
     @Bean("oAuth2ResourceRestTemplate")
     public OAuth2RestTemplate oAuth2RestTemplate(OAuth2ClientContext oAuth2ClientContext) {
-        return new OAuth2RestTemplate(this.oAuth2ProtectedResourceDetails(), oAuth2ClientContext);
+        return new OAuth2RestTemplate(this.clientCredentialResourceDetails(), oAuth2ClientContext);
     }
 
-    private OAuth2ProtectedResourceDetails oAuth2ProtectedResourceDetails() {
+    private OAuth2ProtectedResourceDetails authorizationCodeResourceDetails() {
         AuthorizationCodeResourceDetails resourceDetails = new AuthorizationCodeResourceDetails();
-        resourceDetails.setId("oauth20-demo");
-        resourceDetails.setClientId("sample-client-id");
-        resourceDetails.setClientSecret("sample-client-secret");
-        resourceDetails.setScope(Arrays.asList("read", "write"));
+        resourceDetails.setId(this.resourceId);
+        resourceDetails.setClientId(this.clientId);
+        resourceDetails.setClientSecret(this.clientSecret);
+        resourceDetails.setScope(this.scopes());
         resourceDetails.setAccessTokenUri(this.authorizationServerEndpoint + "/oauth/token");
         resourceDetails.setUserAuthorizationUri(this.authorizationServerEndpoint + "/oauth/authorize");
+        return resourceDetails;
+    }
+
+    private OAuth2ProtectedResourceDetails clientCredentialResourceDetails() {
+        ClientCredentialsResourceDetails resourceDetails = new ClientCredentialsResourceDetails();
+        resourceDetails.setId(this.resourceId);
+        resourceDetails.setClientId(this.clientId);
+        resourceDetails.setClientSecret(this.clientSecret);
+        resourceDetails.setScope(this.scopes());
+        resourceDetails.setAccessTokenUri(this.authorizationServerEndpoint + "/oauth/token");
         return resourceDetails;
     }
 
